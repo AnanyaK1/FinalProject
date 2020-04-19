d806728258b90517c7c587e30cacc0cde4ce6bba
AnanyaK1 <ananyak6@illinois.edu>
2020-04-19 14:11:37 -0500

    Imported default images folder into src

---
+++ app/src/main/java/com/example/finalproject/logic/LoadImageDatabase.java
@@ -0,0 +1,19 @@
+package com.example.finalproject.logic;
+
+package com.javahelps.imagedatabasedemo;
+
+import android.content.Context;
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+//Incase any import statements aren't being recognized, resolve it by clicking File > Invalidate Caches / Restart... > Invalidate and Restart.
+
+//https://www.javahelps.com/2016/04/import-database-with-images-in-android.html
+public class LoadImageDatabase extends SQLiteAssetHelper {
+
+    private static final String DATABASE_NAME = "images.zip";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+}
