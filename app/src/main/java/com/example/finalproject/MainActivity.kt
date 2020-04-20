4fe67edb5ad3baa161dcb43dfe360d05bb675ca1
AnanyaK1 <ananyak6@illinois.edu>
2020-04-20 13:32:30 -0500

    Moved code to Main Activity

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.finalproject
 
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+
+
 /**
  * Represents the main screen of the app, where the user can view and enter games.
  */
@@ -10,5 +16,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        try {
+            ImageView image = (ImageView) findViewById(R.id.test_image);
+            Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.icon);
+            image.setImageBitmap(bMap);
+        } catch (IOException e) {
+                e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/finalproject/logic/Grid.java
+++ app/src/main/java/com/example/finalproject/logic/Grid.java
@@ -1,42 +1,6 @@
 package com.example.finalproject.logic;
 //import javax.imageio.ImageIO;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.widget.ImageView;
-import com.example.finalproject.R;
-import java.io.File;
-import java.io.IOException;
-
-import android.content.Intent;
-import android.graphics.Point;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.TypedValue;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.gson.JsonObject;
-
-import java.util.ArrayList;
-import java.util.List;
-
 //import java.awt.image.BufferedImage;
 
 
@@ -53,24 +17,25 @@ public class Grid {
     }
         //Read more: https://javarevisited.blogspot.com/2011/12/read-write-image-in-java-example.html#ixzz6K07Fhsaj
         //try is used to make sure program won't crash even if exception is spotted
-    public void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); // Required by Android
-        setContentView(R.layout.activity_new_game);
-    }
+//    @Override
+//    public void onCreate(final Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState); // Required by Android
+//        setContentView(R.layout.activity_new_game);
+//    }
 
-        public static void main(String[]args){
-
-            //BufferedImage image = null;
-            //image = ImageIO.read(new File(fileName));
-            try {
-                ImageView image = (ImageView) findViewById(R.id.test_image);
-                Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.icon);
-                image.setImageBitmap(bMap);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+//        public static void main(String[]args){
+//
+//            //BufferedImage image = null;
+//            //image = ImageIO.read(new File(fileName));
+//            try {
+//                ImageView image = (ImageView) findViewById(R.id.test_image);
+//                Bitmap bMap = BitmapFactory.decodeResource(getResources(), R.drawable.icon);
+//                image.setImageBitmap(bMap);
+//
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
 
 
 }
